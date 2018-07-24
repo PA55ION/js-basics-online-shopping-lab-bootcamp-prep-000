b93195ff665951383fdc25108af9508d57bb9bc5
Mai Campbell <jirallc@gmail.com>
2018-07-24 13:54:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ function placeOrder(cardNumber) {
   if(cardNumber) {
     for(let i = 0; i < cart.length; i++) {
       cart.pop();
-      return `"Your total cost is ${total} which will be charged to the card ${cardNumber}."`
+      return `"Your total cost is ${getTotal} which will be charged to the card ${cardNumber}."`
     }
   }
 }
