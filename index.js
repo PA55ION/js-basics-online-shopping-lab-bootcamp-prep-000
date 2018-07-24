ccac8bec86987769a63cf0b838b3681c222d2a4b
Mai Campbell <jirallc@gmail.com>
2018-07-24 14:10:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -50,11 +50,11 @@ function removeFromCart(item) {
     if(cart[i].itemName === item) {
   cart.splice(i, 1);
     return cart;
-    } 
+    }
   }
- 
+
     return 'That item is not in your cart.'
- 
+
 }
 // function removeFromCart(item) {
 //   //write your code here
