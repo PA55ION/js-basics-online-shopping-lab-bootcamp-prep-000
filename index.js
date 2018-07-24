68178204b738347924e73386e3fc381d741899da
Mai Campbell <jirallc@gmail.com>
2018-07-24 14:17:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   // write your code here
   if(cardNumber) {
-    var cart total = total(); 
+    var cartTotal = total();
     for(let i = 0; i < cart.length; i++) {
       cart.pop();
     }
