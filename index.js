b1bd7a25ddbee001a5436f22869bba3e53bfd4b7
Mai Campbell <jirallc@gmail.com>
2018-07-24 14:17:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ function placeOrder(cardNumber) {
     for(let i = 0; i < cart.length; i++) {
       cart.pop();
     }
-      return `Your total cost is $${cart_total}, which will be charged to the card ${cardNumber}`
+      return `Your total cost is $${cart_total}, which will be charged to the card ${cardNumber}.`
     } else {
       return "Sorry, we don't have a credit card on file for you.";
     }
