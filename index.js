a69628ba8b132743bf98d8d64ca02b09e26aad3c
Mai Campbell <jirallc@gmail.com>
2018-07-24 13:54:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -59,9 +59,9 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   // write your code here
   if(cardNumber) {
-    return "Sorry we don't have a credit card on file for you"
-  } else {
     for(let i = 0; i < cart.length; i++) {
+      cart.pop();
+      return `"Your total cost is ${total} which will be charged to the card ${cardNumber}."`
     }
   }
 }
